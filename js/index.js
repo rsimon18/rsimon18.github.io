be6fb3ef0e3d6cdc1b848532ea88ee5bc8cca8ea
rsimon18 <rachel.lin.simon@gmail.com>
2017-03-26 20:09:26 -0400

    Homepage

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$('.social-icons').mouseenter(function() {
+  $(this).css("color", "#cecece")
+});
+
+$('.social-icons').mouseleave(function() {
+  $(this).css("color", "white")
+});
+
+$('p a').mouseenter(function() {
+  $(this).css("color", "black")
+});
+
+$('p a').mouseleave(function() {
+  $(this).css("color", "#999999")
+});
